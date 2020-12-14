6ee5e7188347df54b92923439bee6ea10c1f1f90
ngocthien2306 <75513398+ngocthien2306@users.noreply.github.com>
2020-12-14 14:31:57 +0700

    Update Program.cs

--- Paint_OOP/Program.cs
+++ Paint_OOP/Program.cs
@@ -7,6 +7,7 @@ namespace Paint_OOP
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Project 1");
         }
     }
 }
